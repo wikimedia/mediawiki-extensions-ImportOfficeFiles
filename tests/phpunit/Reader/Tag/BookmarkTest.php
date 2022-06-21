c7f0e8e8d3d630d39a07188c2bacc5b7c8cce7a6
akulbii <alk@astarios.com>
2022-06-21 12:03:47 +0300

    Fix bookmarks processing
    [4.2.x]
    
    ERM27698
    
    Change-Id: Ib4ed2704d32923c6a203b0c9b54464bcfc064b73

--- tests/phpunit/Reader/Tag/BookmarkTest.php
+++ tests/phpunit/Reader/Tag/BookmarkTest.php
@@ -11,15 +11,13 @@ class BookmarkTest extends TagProcessorTestBase {
 	 */
 	protected function performChecks( string $wikiTextReplacement, DOMNodeList $documentElements ) {
 		$bookmarkStart = $documentElements->item( 0 )->getElementsByTagName( 'bookmarkStart' );
-		$bookmarkEnd = $documentElements->item( 0 )->getElementsByTagName( 'bookmarkEnd' );
 		$anchorSpan = $documentElements->item( 0 )->getElementsByTagName( 'span' );
 
 		// "Bookmark start" node should not be removed by "Bookmark" processor
 		// It is replaced with a "<wikitext>" node in conversion process
 		$this->assertSame( 1, $bookmarkStart->length );
 
-		$this->assertSame( 0, $bookmarkEnd->length );
-		$this->assertSame( 1, $anchorSpan->length );
+		$this->assertSame( 1, $anchorSpan->length, 'Anchor span was not found!' );
 
 		$anchorId = $anchorSpan->item( 0 )->getAttribute( 'id' );
 		$this->assertEquals( '_onnpgudyhc4g', $anchorId );
@@ -29,7 +27,7 @@ class BookmarkTest extends TagProcessorTestBase {
 	 * @inheritDoc
 	 */
 	protected function getProcessorName(): string {
-		return 'bookmark';
+		return 'bookmarkStart';
 	}
 
 	/**
