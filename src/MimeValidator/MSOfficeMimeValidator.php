b7b95833c80736f2f3d5dca12ab1f27151e88e51
dvogel <daniel.vogel@hallowelt.com>
2022-06-22 14:12:31 +0200

    Option to disable mime-type check if necessary
    [4.2.x]
    
    ERM27697
    
    Change-Id: I0c8d043fa280b5d865c2b3f84e72ca63db92ae7c

--- src/MimeValidator/MSOfficeMimeValidator.php
+++ src/MimeValidator/MSOfficeMimeValidator.php
@@ -2,12 +2,25 @@
 
 namespace MediaWiki\Extension\ImportOfficeFiles\MimeValidator;
 
+use Config;
 use MediaWiki\Extension\ImportOfficeFiles\IModuleMimeValidator;
 use MediaWiki\MediaWikiServices;
 use MimeAnalyzer;
 use SplFileInfo;
 
 class MSOfficeMimeValidator implements IModuleMimeValidator {
+
+	/**
+	 * @var Config
+	 */
+	private $config;
+
+	/**
+	 * @param Config $config
+	 */
+	public function __construct( Config $config ) {
+		$this->config = $config;
+	}
 
 	/**
 	 * @return array
@@ -23,6 +36,19 @@ class MSOfficeMimeValidator implements IModuleMimeValidator {
 	 * @return bool
 	 */
 	public function canHandle( $file ): bool {
+		$verifyMimeType = $this->config->get( 'VerifyMimeType' );
+
+		if ( $verifyMimeType === true ) {
+			// Mime type check can be disabled because not all docx documents do have the correct mimetype.
+
+			$extension = $file->getExtension();
+			if ( strtolower( $extension ) === 'docx' ) {
+				return true;
+			}
+
+			return false;
+		}
+
 		$services = MediaWikiServices::getInstance();
 		/** @var MimeAnalyzer */
 		$mimeAnalyzer = $services->getService( 'MimeAnalyzer' );
