8fd6a81df3276fcc4024c1a47e6e0960e0f25921
dvogel <daniel.vogel@hallowelt.com>
2022-07-19 11:34:10 +0200

    Remove unused injection for wikitext processors
    [4.2.x]
    
    ERM29020
    
    Change-Id: If4bbda90369926ddd9cc9ef4f0a13bed3362ff56

--- src/WikiTextProcessor/BoldMarkupReplacement.php
+++ src/WikiTextProcessor/BoldMarkupReplacement.php
@@ -3,21 +3,8 @@
 namespace MediaWiki\Extension\ImportOfficeFiles\WikiTextProcessor;
 
 use MediaWiki\Extension\ImportOfficeFiles\IWikiTextProcessor;
-use MediaWiki\Extension\ImportOfficeFiles\Workspace;
 
 class BoldMarkupReplacement implements IWikiTextProcessor {
-
-	/**
-	 * @var Workspace
-	 */
-	private $workspace;
-
-	/**
-	 * @param Workspace $workspace
-	 */
-	public function __construct( Workspace $workspace ) {
-		$this->workspace = $workspace;
-	}
 
 	/**
 	 * @param string $wikiText
