1a0ee737257a3640065c03ebbc6a10f96f46464b
dvogel <daniel.vogel@hallowelt.com>
2022-10-19 16:52:56 +0200

    Fix removing page title as heading from content text
    [4.2.x]
    
    ERM29988
    
    Change-Id: Ibf7163a6803bffcabf38ccaed9728d6ad1d8791e

--- tests/phpunit/RemoveHeadingTest.php
+++ tests/phpunit/RemoveHeadingTest.php
@@ -5,6 +5,7 @@ namespace MediaWiki\Extension\ImportOfficeFiles\Tests\Reader\PostProcessor;
 use MediaWiki\Extension\ImportOfficeFiles\RemoveHeading;
 use PHPUnit\Framework\TestCase;
 
+// phpcs:disable Generic.Files.LineLength.TooLong
 /**
  * @covers \MediaWiki\Extension\ImportOfficeFiles\TitleMapBulderTest
  */
@@ -15,7 +16,7 @@ class RemoveHeadingTest extends TestCase {
 	public function testExecute() {
 		$removeHeading = new RemoveHeading();
 
-		$expectedWikiText = "\nHello world!\n===Second heading===\nHello again\n===My heading===\nTest";
+		$expectedWikiText = "\nHello world!\n===Second heading 1===\nHello again\n===My heading===\nTest";
 		$wikiText = '==<span style="abc:def">' . 'My heading</span>==' . "$expectedWikiText";
 
 		$actualWikiText = $removeHeading->execute(
@@ -26,12 +27,45 @@ class RemoveHeadingTest extends TestCase {
 
 		$this->assertEquals( $expectedWikiText, $actualWikiText );
 
-		$expectedWikiText = "\nHello world!\n===Second heading===\nHello again\n===My heading 2===\nTest";
+		$expectedWikiText = "\nHello world!\n===Second heading 2===\nHello again\n===My heading 2===\nTest";
 		$wikiText = '===<span style="abc:def">' . 'My heading</span>===' . "$expectedWikiText";
 
 		$actualWikiText = $removeHeading->execute(
 			'3',
 			'TestCase:Heading test/My heading',
+			$wikiText
+		);
+
+		$this->assertEquals( $expectedWikiText, $actualWikiText );
+
+		$expectedWikiText = "\nHello world!\n===Second heading 3===\nHello again\n===My heading 2===\nTest";
+		$wikiText = '=== <span class="bookmark-start" id="_Toc85796086"></span>' . 'My heading===' . "$expectedWikiText";
+
+		$actualWikiText = $removeHeading->execute(
+			'3',
+			'TestCase:Heading test/My heading',
+			$wikiText
+		);
+
+		$this->assertEquals( $expectedWikiText, $actualWikiText );
+
+		$expectedWikiText = "\nHello world!\n===Second heading 4===\nHello again\n===My heading 2===\nTest";
+		$wikiText = '=== <span style="abc:def">  <span class="bookmark-start" id="_Toc85796086"></span>' . 'My heading</span> ===' . "$expectedWikiText";
+
+		$actualWikiText = $removeHeading->execute(
+			'3',
+			'TestCase:Heading test/My heading',
+			$wikiText
+		);
+
+		$this->assertEquals( $expectedWikiText, $actualWikiText );
+
+		$expectedWikiText = "\nHello world!\n===Second heading 5===\nHello again\n===My heading 2===\nTest";
+		$wikiText = '=== <span style="abc:def">  <span class="bookmark-start" id="_Toc85796086"></span>' . 'My heading/Ivalid character</span> ===' . "$expectedWikiText";
+
+		$actualWikiText = $removeHeading->execute(
+			'3',
+			'TestCase:Heading test/My heading Ivalid character',
 			$wikiText
 		);
 
