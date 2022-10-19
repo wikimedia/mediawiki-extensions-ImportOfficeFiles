f36474bdca5331cfa29b18dd7942a6588ba436e8
dvogel <daniel.vogel@hallowelt.com>
2022-10-19 16:52:56 +0200

    Fix removing page title as heading from content text
    [4.2.x]
    
    ERM29988
    
    Change-Id: Ibf7163a6803bffcabf38ccaed9728d6ad1d8791e

--- src/RemoveHeading.php
+++ src/RemoveHeading.php
@@ -5,6 +5,11 @@ namespace MediaWiki\Extension\ImportOfficeFiles;
 class RemoveHeading {
 
 	/**
+	 * @var string
+	 */
+	private $whiteSpaceRegEx = '[:#<>\s\/\t\n\[\]\{\}\|]';
+
+	/**
 	 * @param string $level
 	 * @param string $heading
 	 * @param string $wikiText
@@ -14,17 +19,19 @@ class RemoveHeading {
 		$sanitizer = new SanitizeHeading();
 		$sanitizedWikiText = $sanitizer->execute( $level, $wikiText );
 
-		$pos = strpos( $heading, ':' );
-		if ( $pos ) {
-			$heading = substr( $heading, $pos + 1 );
-		}
+		$titleParts = explode( ':', $heading );
+		$heading = array_pop( $titleParts );
 
 		$headingParts = explode( '/', $heading );
-		if ( $headingParts > 1 ) {
-			$heading = array_pop( $headingParts );
-		}
+		$heading = array_pop( $headingParts );
 
-		$regEx = "/^={" . $level . "}(" . $heading . ")={" . $level . "}/m";
+		// Prepare $heading text for regex
+		$quotedHeading = preg_quote( $heading );
+		/* In $heading invalid characters are replaced by ' '. To match them in
+		 * wikitext the ' ' is replaced by a regex
+		 */
+		$quotedHeading = str_replace( ' ', $this->whiteSpaceRegEx, $quotedHeading );
+		$regEx = "/^={" . $level . "}\s*" . $quotedHeading . "\s*={" . $level . "}/m";
 		$newWikiText = preg_replace( $regEx, '', $sanitizedWikiText );
 
 		if ( !$newWikiText ) {
