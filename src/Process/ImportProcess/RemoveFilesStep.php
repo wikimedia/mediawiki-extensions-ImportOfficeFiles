d514a798cacbbe21adec9c136a7dae5be880c99f
hslater <hamish.slater@hallowelt.com>
2023-12-20 01:24:54 +1300

    Make compatible with LoggerAwareInterface::setLogger()
    ERM29303
    
    [REL1_39, REL1_39-2.0.x, master]
    
    Change-Id: Ibc8a65935d69f0238e6a44287f66f7036aa85501
    (cherry picked from commit 7a11485703af813bbe3330b72347d05af2107a7c)

--- src/Process/ImportProcess/RemoveFilesStep.php
+++ src/Process/ImportProcess/RemoveFilesStep.php
@@ -36,7 +36,7 @@ class RemoveFilesStep implements InterruptingProcessStep, LoggerAwareInterface {
 	 * @param LoggerInterface $logger
 	 * @return void
 	 */
-	public function setLogger( LoggerInterface $logger ) {
+	public function setLogger( LoggerInterface $logger ): void {
 		$this->logger = $logger;
 	}
 
